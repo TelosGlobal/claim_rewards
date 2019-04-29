8289a3689cf7f6c0a993d2cb8ad644675d7045e6
Kevin Quaintance <kevin@quaintance.net>
2019-04-29 10:48:37 -0700

    updated to cleos command

--- claimrewards.sh
+++ claimrewards.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 date
 #Unlock wallet
-/FULL_PATH/teclos wallet unlock -n WALLET_NAME --password YOUR_WALLET_PASSWORD
+/FULL_PATH/cleos wallet unlock -n WALLET_NAME --password YOUR_WALLET_PASSWORD
 #Claim
-/FULL_PATH/teclos push action eosio claimrewards '{"owner":"ACCOUNT_NAME"}' -p ACCOUNT_NAME@claimer
-/FULL_PATH/teclos wallet lock -n WALLET_NAME
+/FULL_PATH/cleos push action eosio claimrewards '{"owner":"ACCOUNT_NAME"}' -p ACCOUNT_NAME@claimer
+/FULL_PATH/cleos wallet lock -n WALLET_NAME
